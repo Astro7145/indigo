e7ecea5ce2a48017a97e0b60f8233e9ea9b2a0cd
GuNyangE <noel7145@gmail.com>
2026-05-20 13:33:32 +0900

    fix: 아이콘 변경 및 텍스트 색상 변경

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -5,6 +5,7 @@ import { AnimatePresence, motion } from 'motion/react'
 import { useToastStore } from '@/src/stores/toast'
 
 import ToastPortal from '../portals/ToastPortal'
+import { IcCheck } from '../icons'
 
 export default function Toast() {
   const { isOpen, message } = useToastStore()
@@ -21,9 +22,11 @@ export default function Toast() {
             transition={{ duration: 0.25, ease: 'easeOut' }}
             className="fixed bottom-6 left-1/2 flex -translate-x-1/2 items-center gap-x-1 rounded-sm bg-indigo-200 px-4 py-2 shadow-lg"
           >
-            <span>✅</span>
+            <IcCheck color="indigo" className="size-6" />
             {/* 1초전 이라는 문구는 큰 의미가 없어 보여 추가하지 않고 대신 메세지를 자유롭게 입력 가능하도록 구현함 */}
-            <span className="text-sm font-semibold">{message}</span>
+            <span className="text-sm font-semibold text-indigo-600">
+              {message}
+            </span>
           </motion.div>
         )}
       </AnimatePresence>
