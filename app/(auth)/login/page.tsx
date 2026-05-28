05cfe8fe97e066335b00b166042c5134b0057f8d
GuNyangE <noel7145@gmail.com>
2026-05-28 11:37:06 +0900

    fix: 페이지 컴포넌트 이름 변경

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import { LogoFull } from '@/src/components/common/icons';
 import LoginForm from '@/src/components/auth/LoginForm';
 import SocialLoginSection from '@/src/components/auth/SocialLoginSection';
 
-export default function LoginPage() {
+export default function Page() {
   return (
     <div className="flex w-full max-w-100 flex-col gap-10">
       {/* 로고 */}
